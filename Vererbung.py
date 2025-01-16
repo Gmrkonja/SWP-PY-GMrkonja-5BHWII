4e0c111b36e95987ad4cb50c41a3d44489e760cd
Gmrkonja <125956433+Gmrkonja@users.noreply.github.com>
2025-01-16 13:37:54 +0100

    Add files via upload

--- Vererbung.py
+++ Vererbung.py
@@ -1,21 +1,36 @@
 class Genre:
-    def __init__(self, genre):
-        self.genre = genre
+    def __init__(self, bezeichnung):
+        if not isinstance(bezeichnung, str):
+            raise ValueError("Wurde kein bezeichnung gegeben!!")
+        self.bezeichnung = bezeichnung
     
     def __str__(self):
-        return f"{self.genre}"
+        return f"{self.bezeichnung}"
     
 
 class Spiele(Genre):
-    def __init__(self, name, jahr, firma, genre):
+    def __init__(self, name, jahr, firma, bezeichnung):
         self.name = name
         self.jahr = jahr
         self.firma = firma
-        super().__init__(genre)
-    
+        super().__init__(bezeichnung)
+
+
     def __str__(self):
         return f"{self.name}({self.jahr}) von {self.firma} ist ein {super().__str__()}"
-    
+
+
+try:
+    Shooter = Genre(12)
+    print(Shooter)
+except Exception as Error:
+    print(Error)
+
+try:
+    Fighting = Genre("Fighting")
+    print(Fighting)
+except Exception as Error:
+    print(Error)
 
 Tekken8 = Spiele("Tekken 8", 2024, "Bandai Namco", "Fighting Game")
 print(Tekken8)
